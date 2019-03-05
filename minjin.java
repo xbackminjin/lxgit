cbaa46c2c9de7109d99369b3bb408569c7fd3b03
xbackminjin <48210449+xbackminjin@users.noreply.github.com>
2019-03-05 10:49:02 +0800

    修改java文件

--- minjin.java
+++ minjin.java
@@ -1,1 +1,14 @@
-minjin111111
+public static void main(String[] args) {
+        WeatherSubject weatherSubject = new WeatherSubject();
+        TravelSubject travelSubject = new TravelSubject();
+
+        Person list = new Person(weatherSubject,travelSubject);
+
+
+        weatherSubject.setDay("1007");
+        weatherSubject.setMaxTmp(12);
+        weatherSubject.setMinTmp(-5);
+
+
+        weatherSubject.notifyObservers();
+    }
